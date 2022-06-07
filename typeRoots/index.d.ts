6bdc59a1fb97f57d1c87d05e064ce4f83387c93f
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-06-07 19:42:39 -0300

    goddamn TS

--- typeRoots/index.d.ts
+++ typeRoots/index.d.ts
@@ -1,14 +1,12 @@
 // eslint-disable-next-line max-classes-per-file
 declare module 'sib-api-v3-sdk' {
-  type Cc = import('sib-api-v3-typescript').CreateContact;
-  type Ca = import('sib-api-v3-typescript').ContactsApi;
-  class ContactsApi implements Ca {
+  class ContactsApi {
     public createContact(
       createContact: CreateContact,
       options?: { headers: { [name: string]: string; };
       }): Promise<{ response: IncomingMessage; body: CreateUpdateContactModel; }>;
   }
-  class CreateContact implements Cc {
+  class CreateContact {
     email: string;
 
     attributes: object;
